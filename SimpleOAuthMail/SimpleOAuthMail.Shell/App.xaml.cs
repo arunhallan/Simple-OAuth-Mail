6e19dadcd8ae7e9c23a1485f4b21214eecd0157e
arunhallan <arun.hallan@gmail.com>
2014-05-08 11:25:53 +0800

    Remove code to catch all errors
    Remove code to catch all errors. Will need to implement a proper
    solution to exception management in the future.

--- SimpleOAuthMail/SimpleOAuthMail.Shell/App.xaml.cs
+++ SimpleOAuthMail/SimpleOAuthMail.Shell/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.IO;
 using System.Windows;
 
 namespace SimpleOAuthMail.Shell
@@ -10,45 +10,9 @@ namespace SimpleOAuthMail.Shell
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            #if DEBUG
-                RunInDebugMode();
-            #else
-                RunInReleaseMode();
-            #endif    
-        }
-
-        private static void RunInDebugMode()
-        {
+            base.OnStartup(e);
             SimpleOAuthMailBootstrapper bootstrapper = new SimpleOAuthMailBootstrapper();
             bootstrapper.Run();
         }
-
-        private static void RunInReleaseMode()
-        {
-            AppDomain.CurrentDomain.UnhandledException += AppDomainUnhandledException;
-            try
-            {
-                SimpleOAuthMailBootstrapper bootstrapper = new SimpleOAuthMailBootstrapper();
-                bootstrapper.Run();
-            }
-            catch (Exception ex)
-            {
-                HandleException(ex);
-            }
-        }
-
-        private static void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            HandleException(e.ExceptionObject as Exception);
-        }
-
-        private static void HandleException(Exception ex)
-        {
-            if (ex == null) return;
-
-            MessageBox.Show(ex.Message);
-            Environment.Exit(1);
-        }
     }
-    
 }
