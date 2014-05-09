ed42678e64987b10992852348ac46db70c97bda1
arunhallan <arun.hallan@gmail.com>
2014-05-09 13:27:09 +0800

    Improvements made to FacebookMessageFactory
    Better Json Parsing

--- SimpleOAuthMail/SimpleOAuthMail.OAuthDataConnections/Facebook/FacebookMessageFactory.cs
+++ SimpleOAuthMail/SimpleOAuthMail.OAuthDataConnections/Facebook/FacebookMessageFactory.cs
@@ -13,21 +13,15 @@ namespace SimpleOAuthMail.OAuthDataConnections.Facebook
 
             foreach (var token in message)
             {
-                try
+                if (token.Key == "data")
                 {
-                    if (token.Key == "data")
+                    foreach (var tokenL1 in token.Value)
                     {
-                        foreach (var tokenL1 in token.Value)
-                        {
-                            ICommonMailMessage commonMailMessage = ExtractMessageFromJson(tokenL1);
+                        ICommonMailMessage commonMailMessage = ExtractMessageFromJson(tokenL1);
 
-                            if (!string.IsNullOrEmpty(commonMailMessage.Body))
-                                commonMailMessages.Add(commonMailMessage);
-                        }
+                        if (!string.IsNullOrEmpty(commonMailMessage.Body))
+                            commonMailMessages.Add(commonMailMessage);
                     }
-                }
-                catch 
-                {
                 }
             }
 
@@ -41,8 +35,6 @@ namespace SimpleOAuthMail.OAuthDataConnections.Facebook
         /// <returns></returns>
         private static ICommonMailMessage ExtractMessageFromJson(JToken token)
         {
-            string body = string.Empty;
-
             JToken toToken = token.Value<JToken>("to");
 
             List<string> toItems = (from toData in toToken 
@@ -50,13 +42,34 @@ namespace SimpleOAuthMail.OAuthDataConnections.Facebook
                                             from toItem in individualTo 
                                                 select toItem.Value<string>("name")).ToList();
 
+            List<string> bodyComments = new List<string>();
             JToken comments = token.Value<JToken>("comments");
             if (comments != null)
             {
-                body = comments.ToString();
+                var commentProperties = (from comment in comments 
+                                                from subComment in comment 
+                                                    from subSubComment in subComment 
+                                                        select (subSubComment));
+
+                foreach (var commentProperty in commentProperties)
+                {
+                    if (!(commentProperty is JProperty))
+                    {
+                        string message = commentProperty.Value<string>("message");
+                        JObject fromObject = commentProperty.Value<JObject>("from");
+                        if (fromObject != null)
+                        {
+                            string messageFrom = fromObject.Value<string>("name");
+                            message = string.Format("{0}:\n{1}", messageFrom, message);
+                        }
+
+                        bodyComments.Add(message);
+                    }
+                }
+
             }
-            
-            
+
+            string body = string.Join("\n", bodyComments);
             return new CommonMailMessage(toItems, string.Empty, body, string.Empty);
         }
     }
